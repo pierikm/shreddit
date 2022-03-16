b72f755b226f384f4d08264ceb99722633a1a7c8
pierikm <pierikm@umich.edu>
2022-03-16 12:29:23 -0400

    refactorted

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,7 +5,7 @@ import LoginForm from './components/auth/LoginForm';
 import SignUpForm from './components/auth/SignUpForm';
 import NavBar from './components/NavBar';
 import ProtectedRoute from './components/auth/ProtectedRoute';
-import UsersList from './components/UsersList';
+import UsersList from './components/User/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
 
@@ -42,6 +42,9 @@ function App() {
         </ProtectedRoute>
         <ProtectedRoute path='/' exact={true} >
           <h1>My Home Page</h1>
+        </ProtectedRoute>
+        <ProtectedRoute>
+
         </ProtectedRoute>
       </Switch>
     </BrowserRouter>
