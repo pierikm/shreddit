20296e10c472c0a582a4de1d9d2f0faaf3efc288
pierikm <pierikm@umich.edu>
2022-04-14 14:37:01 -0400

    button spacing

--- react-app/src/components/Post/index.js
+++ react-app/src/components/Post/index.js
@@ -96,9 +96,9 @@ function Post() {
                                 </a>
                             }
                             <span className="preview-user-btns">
-                            <span className="preview-posted-by">posted by</span>
-                            <span className="preview-username">{post.username}</span>
-                            <span>{post.timestamp} </span>
+                                <span className="preview-posted-by">posted by</span>
+                                <span className="preview-username">{post.username}</span>
+                                <span>{post.timestamp}</span>
                                 {user?.id === post?.user_id &&
                                     <>
                                         <button
